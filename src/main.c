e6db958f1932e8d945e8a5828bf79410202aa58b
dhia1999 <ge38qal@tum.de>
2022-10-26 17:10:23 +0000

    Coding challenge 2 done.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "espl_lib.h"
 
-int main (){
+int main ()
+{
 
-printf("Hello ESPL");
+unsigned int Given_Number;
+char response;
+
+do 
+{
+printf("Enter a number: ");
+scanf("%d", &Given_Number);
+printf("%s\n", num_to_words(Given_Number));
+
+printf("You want to give another Number? (Y/N) :");
+scanf(" %c", &response);
+}while (response=='Y');
 
 return 0;
+
 
 }
 
